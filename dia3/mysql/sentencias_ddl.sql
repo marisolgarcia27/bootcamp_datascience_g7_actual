081d6ddf3562c2786542e07e158c9ed64017d29a
marisol garcia <gmarisolalessandra@gmail.com>
2026-03-16 21:50:53 -0500

    sql

--- dia3/mysql/sentencias_ddl.sql
+++ dia3/mysql/sentencias_ddl.sql
@@ -1,51 +1,22 @@
--- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: db_matriculas
--- ------------------------------------------------------
--- Server version	8.0.45
+CREATE DATABASE db_matriculas
+    DEFAULT CHARACTER SET = 'utf8mb4';
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+show databases;
+use db_matriculas;
+show tables;
 
---
--- Table structure for table `alumnos`
---
+-- SENTENCIAS DDL
+-- CREAR UNA TABLA
+CREATE TABLE alumno(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nro_documento VARCHAR(20) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    email VARCHAR(100)
+);
 
-DROP TABLE IF EXISTS `alumnos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `alumnos` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nro_documento` varchar(50) NOT NULL,
-  `nombre` varchar(225) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- MODIFICAR UNA TABLA
+ALTER TABLE alumno
+ADD COLUMN nota INT default 0;
 
---
--- Dumping data for table `alumnos`
---
-
-/*!40000 ALTER TABLE `alumnos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `alumnos` ENABLE KEYS */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2026-03-16 21:33:23
+-- ELIMINAR TABLA
+DROP TABLE alumno;
